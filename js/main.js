4e0e83ea8844f2e99d29f0062eee3c4a22086fa0
James Mutahi <mutahijames0@gmail.com>
2019-05-12 16:05:29 +0300

    Set up constructor, prototypes and random number function

--- js/main.js
+++ js/main.js
@@ -8,3 +8,31 @@ function showPage() {
     document.getElementById("mdb-preloader").style.display = "none";
     document.getElementById("myDiv").style.display = "block";
 }
+
+var num = 0,
+    newMark = 0,
+    playerDetails = [],
+    finalScore = 0,
+    diePic = "",
+    pos = 0;
+
+function PlayersInfo(name, score, totalScore) {
+    this.playerNames = name;
+    this.playerMarks = score;
+    this.totalScores = totalScore;
+}
+var genRandom = function() {
+    randomNo = Math.floor(Math.random() * 6) + 1;
+    return randomNo;
+}
+PlayersInfo.prototype.AddScores = function(thisMark) {
+    if (thisMark === 1) {
+        this.playerMarks = 0;
+    } else if (thisMark !== 1) {
+        this.playerMarks = this.playerMarks + thisMark;
+    }
+    return this.playerMarks;
+}
+PlayersInfo.prototype.Total = function(total) {
+    return this.totalScores = this.totalScores + total;
+}
